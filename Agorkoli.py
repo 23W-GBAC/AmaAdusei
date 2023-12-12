c6f45a6c70158b95a64f34ea646947297d0982b6
Ama <148862738+AmaAdusei@users.noreply.github.com>
2023-12-12 10:42:34 +0100

    Update Agorkoli.py

--- Agorkoli.py
+++ Agorkoli.py
@@ -3,16 +3,12 @@ from bs4 import BeautifulSoup
 
 def get_Agorkoli(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser') 
+    headlines = soup.find_all('h2')
 
-   headlines = soup.find_all('h2')
-
-for index, headline in enumerate(headlines, start=1):
-    print(f"{index}. {headline.text.strip()}")
+    for index, headline in enumerate(headlines, start=1):
+        print(f"{index}. {headline.text.strip()}")
 
 if __name__ == "__main__":
-    # Your main code or additional logic can go here
-
-    news_url = " https://www.verywellmind.com"
+    news_url = "https://www.verywellmind.com"  # Removed the space before the URL
     get_Agorkoli(news_url)
-
