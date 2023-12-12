cfd9571dc372e9ae9d17f21daf2ef0c16df1a12c
Ama <148862738+AmaAdusei@users.noreply.github.com>
2023-12-12 10:35:33 +0100

    Update Agorkoli.py

--- Agorkoli.py
+++ Agorkoli.py
@@ -5,11 +5,14 @@ def get_Agorkoli(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    headlines = soup.find_all('h2')
+   headlines = soup.find_all('h2')
 
-    for index, headlines in enumerate(headlines, start=1):
-        print(f"{index}. {headline.text.strip()}")
+for index, headline in enumerate(headlines, start=1):
+    print(f"{index}. {headline.text.strip()}")
+
 if __name__ == "__main__":
+    # Your main code or additional logic can go here
+
     news_url = " https://www.verywellmind.com"
     get_Agorkoli(news_url)
 
