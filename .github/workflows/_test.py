f66cee324587fdf06811459e91ee37d268233fcb
Ama <148862738+AmaAdusei@users.noreply.github.com>
2023-12-14 17:55:36 +0100

    Delete .github/workflows directory

--- .github/workflows/_test.py
+++
@@ -1,8 +0,0 @@
-# File: test_example.py
-
-def test_addition():
-    assert 5 + 1 == 6
-
-def test_subtraction():
-    assert 3 - 1 == 2
-pytest _test.py
--- .github/workflows/test_example.py
+++
@@ -1,7 +0,0 @@
-# File: test_example.py
-
-def test_addition():
-    assert 1 + 1 == 2
-
-def test_subtraction():
-    assert 3 - 1 == 2
